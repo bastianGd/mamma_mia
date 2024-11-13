1a0b5e9132ad6f9c896f6c8c38ad65953f96ec01
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-12 23:59:14 -0300

    add: Header

---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./header.css"
+
+const Header = () => {
+    return (
+        <>
+            <div className="hero-seccion d-flex justify-content-center align-items-center flex-column">
+                <h1 className="hero-title" id="title">
+                    ¡Pizzería Mamma Mia!
+                </h1>
+                <h6 className="hero-subtitle" id="subtitle">
+                    ¡Tenemos las mejores pizzas que podrás encontrar!
+                </h6>
+                <hr className="w-50" />
+            </div>
+        </>
+    )
+}
+
+export default Header
