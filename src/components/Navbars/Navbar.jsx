09a1e546c5095620862eafa82fc007f24e976406
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-12 21:51:06 -0300

    add navbar component

---
+++ src/components/Navbars/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Nav, Navbar } from "react-bootstrap";
+// import formatNumber from "../../Utils/formateo/Format";
+import pizzaLogo from "../../assets/imgs/Navbar/logo-pizzeria.png";
+
+const NavbarPizza = () => {
+    const token = 1;
+    // const total = 15000;
+
+    return (
+        <Navbar bg="dark" className="px-5" data-bs-theme="dark">
+            <Navbar.Brand className="mx-3">
+                <img
+                    src={pizzaLogo}
+                    alt="logo pizzería mamma mia"
+                    height="50"
+                    className="d-inline-block align-top"
+                />
+            </Navbar.Brand>
+            <Nav variant="tabs" defaultActiveKey="#" className="me-auto">
+                <Nav.Link href="#">🍕 Home</Nav.Link>
+                {token ? (
+                    <>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-1">👤 Profile</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-2">🔒 Logout</Nav.Link>
+                        </Nav.Item>
+                    </>
+                ) : (
+                    <>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-3">🔑 Login</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-4">🔐 Register</Nav.Link>
+                        </Nav.Item>
+                    </>
+                )}
+            </Nav>
+            {token && (
+                <Navbar.Brand className="border border-primary text-info px-3">
+                    {/* 🛒 Total: $ {formatNumber(total)} */}0
+                </Navbar.Brand>
+            )}
+        </Navbar>
+    );
+};
+
+export default NavbarPizza;
