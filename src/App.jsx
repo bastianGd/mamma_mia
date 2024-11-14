7a33d6df07d24cc5f26e16a223a763d355b97a3c
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-14 20:24:27 -0300

    add final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import CustomCard from './components/Cards/customCard'
 import Navbar from './components/Navbars/Navbar'
+import Footer from './pages/Footer/FooterPizza'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Home/>
+    <CustomCard/>
+    <Footer/>
     </>
   )
 }
