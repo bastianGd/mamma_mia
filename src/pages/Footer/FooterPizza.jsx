77dabab1eb539a4a9abb79a3669332815f6996b0
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-14 20:24:06 -0300

    add Footer Component

---
+++ src/pages/Footer/FooterPizza.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      © 2024 - Pizzería Mamma Mia! - Todos los derechos reservados - Página Creada por Bastián Gabrielli Desafío Latam
+    </footer>
+  );
+};
+
+export default Footer;
