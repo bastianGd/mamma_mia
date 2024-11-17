619dc4e3aac852f0e4ce17e0d3df0adceeebfe31
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-17 14:44:41 -0300

    add registerForm y logica

---
+++ src/components/Forms/LoginForm/InputLogin.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import LoginForm from "./LoginForm"
+import ValidationForm from "../FormUtils/ValidationLogin"
+import Swal from 'sweetalert2'
+
+
+const InputForm = () => {
+    //Comportamiento y estados
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState({});
+
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        //prevenir el envío del formulario
+        e.preventDefault();
+
+        const validationErrors = ValidationForm(formData);
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length === 0) {
+            console.log('Formulario válido, enviando datos...', formData);
+            //Mensaje de cuenta creada
+            Swal.fire({
+                title: "Usuario Comprobado",
+                text: "Los datos han sido validados satisfactoriamenmte",
+                icon: "success",
+            });
+
+            //Reseteo del formulario
+            setFormData({
+                email: "",
+                password: "",
+            })
+        } else {
+            console.log('Errores de validación:', validationErrors);
+        }
+    }
+
+    return (
+        <LoginForm
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            formData={formData}
+            errors={errors}>
+        </LoginForm>
+    )
+}
+
+export default InputForm
---
+++ src/components/Forms/RegisterForm.jsx/RegisterForm.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+import Form from "react-bootstrap/Form";
+import CustomButton from "../../Buttons/CustomButton";
+
+const RegisterForm = ({ handleChange, handleSubmit, formData, errors }) => {
+  return (
+    <Form className="container my-5 col-md-6" onSubmit={handleSubmit}>
+      {/* Campo de correo electrónico */}
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Dirección de correo electrónico</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Ingresa tu correo electrónico"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <Form.Text className="text-danger">{errors.email}</Form.Text>
+      </Form.Group>
+
+      {/* Campo de contraseña */}
+      <Form.Group className="mb-3" controlId="formPassword">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Ingresa una contraseña"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <Form.Text className="text-danger">{errors.password}</Form.Text>
+      </Form.Group>
+
+      {/* Campo de confirmación de contraseña */}
+      <Form.Group className="mb-3" controlId="formPasswordConfirm">
+        <Form.Label>Confirmar Contraseña</Form.Label>
+        <Form.Control
+          type="password"
+          name="passwordConfirm"
+          placeholder="Ingresa nuevamente tu contraseña"
+          value={formData.passwordConfirm}
+          onChange={handleChange}/>
+        <Form.Text className="text-danger">{errors.passwordConfirm}</Form.Text>
+      </Form.Group>
+
+      {/* Botón personalizado */}
+      <CustomButton color="success" label={"🍕Crear Cuenta"} type="submit" />
+    </Form>
+  );
+};
+
+export default RegisterForm;
