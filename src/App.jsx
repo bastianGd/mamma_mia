89010d1c1464736ba6e7fcd17eee261d1125e8ac
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-17 14:42:06 -0300

    add id - class para css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
-import CustomCard from './components/Cards/customCard'
+import InputForm from './components/Forms/RegisterForm.jsx/InputForm'
 import Navbar from './components/Navbars/Navbar'
 import Footer from './pages/Footer/FooterPizza'
 import Home from './pages/Home/Home'
+import InputLogin from './components/Forms/LoginForm/InputLogin'
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <CustomCard/>
-    <Footer/>
+      <div id="root">
+        <div className='main-content'>
+          <Navbar />
+          {/* <Home/> */}
+          {/* <InputForm/> */}
+          <InputLogin />
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
