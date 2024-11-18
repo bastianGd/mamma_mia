d9ee3482c6d994c2c16b1d83323f6d09bcd8c056
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-17 23:49:05 -0300

    returned back button function

--- src/components/Buttons/CustomButton.jsx
+++ src/components/Buttons/CustomButton.jsx
@@ -1,11 +1,11 @@
 import Button from 'react-bootstrap/Button';
 
 // eslint-disable-next-line react/prop-types
-const CustomButton = ({ color, label, onClick }) => {
+const CustomButton = ({ color, label, action }) => {
   return (
     <Button
       variant={color}
-      onClick={onClick}>
+      type={action}>
       {label}
     </Button>
   );
