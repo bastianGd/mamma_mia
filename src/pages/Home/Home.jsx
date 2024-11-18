bf9671f02ca57e57f526bc2e14a75b658ded8600
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-17 22:34:09 -0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
 import Header from "../Header/Header";
-import CustomCard from "../../components/Cards/CustomCard"
+import CustomCard from "../../components/Cards/CustomCard";
+import pizzas from "../../data/pizzas.js";
 
 const Home = () => {
+
   return (
     <>
-    <Header/>
-    <CustomCard/>
+      <Header />
+      <div className="d-flex flex-wrap justify-content-center gap-5">
+        {pizzas.map((pizza) => (
+          <CustomCard key={pizza.id} pizza={pizza} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
