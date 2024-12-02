ebbb7853ed60b21dfebf6a2e75dffb037f82745d
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-12-01 22:43:21 -0300

    add Link router

--- src/components/Navbars/Navbar.jsx
+++ src/components/Navbars/Navbar.jsx
@@ -1,14 +1,16 @@
 import { Nav, Navbar } from "react-bootstrap";
-import formatNumber from "../../utils/FormatNumber"
+import formatNumber from "@/utils/FormatNumber";
 import pizzaLogo from "@/assets/imgs/Navbar/logo-pizzeria.png";
+import { Link } from "react-router-dom";
 
 const NavbarPizza = () => {
-    const token = 1;
+    const token = true; 
     const total = 5980;
 
     return (
-        <Navbar bg="dark" className="px-5" data-bs-theme="dark">
-            <Navbar.Brand className="mx-3">
+        <Navbar bg="dark" expand="lg" className="px-5" data-bs-theme="dark">
+            {/* Logo de la pizzería */}
+            <Navbar.Brand as={Link} to="/" className="mx-3">
                 <img
                     src={pizzaLogo}
                     alt="logo pizzería mamma mia"
@@ -16,32 +18,28 @@ const NavbarPizza = () => {
                     className="d-inline-block align-top"
                 />
             </Navbar.Brand>
-            <Nav variant="tabs" defaultActiveKey="#" className="me-auto">
-                <Nav.Link href="#">🍕 Home</Nav.Link>
+
+            {/* Navegación principal */}
+            <Nav variant="tabs" defaultActiveKey="/" className="me-auto">
+                <Nav.Link as={Link} to="/">🍕 Home</Nav.Link>
                 {token ? (
                     <>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-1">👤 Profile</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-2">🔒 Logout</Nav.Link>
-                        </Nav.Item>
+                        <Nav.Link as={Link} to="/profile">👤 Profile</Nav.Link>
+                        <Nav.Link as={Link} to="/logout">🔒 Logout</Nav.Link>
                     </>
                 ) : (
                     <>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-3">🔑 Login</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-4">🔐 Register</Nav.Link>
-                        </Nav.Item>
+                        <Nav.Link as={Link} to="/login">🔑 Login</Nav.Link>
+                        <Nav.Link as={Link} to="/register">🔐 Register</Nav.Link>
                     </>
                 )}
             </Nav>
+
+            {/* Total del carrito (solo si está logueado) */}
             {token && (
-                <Navbar.Brand className="border border-primary text-info px-3">
+                <Nav.Link as={Link} to='/cart' className="border border-primary text-info px-3">
                     🛒 Total: $ {formatNumber(total)}
-                </Navbar.Brand>
+                </Nav.Link>
             )}
         </Navbar>
     );
