aa7a7eab15adefe5fd2378852e9aec791145ecad
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-12-01 22:43:33 -0300

    add pizzas api

---
+++ src/utils/APIs/Pizzas.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import CustomCard from "../../components/Cards/CustomCard";
+
+const Pizza = () => {
+    const [pizzas, setPizzas] = useState([]);
+    const URL = "http://localhost:5000/api/pizzas";
+
+    const fetchPizzas = async () => {
+        try {
+            const response = await fetch(URL);
+            const data = await response.json();
+            setPizzas(data);
+        } catch (error) {
+            console.error("Error fetching pizza data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPizzas();
+    }, []);
+
+    if (pizzas.length === 0) {
+        return <div>Cargando información, por favor, espere...</div>;
+    }
+
+    return (
+        <div className="d-flex flex-wrap justify-content-center m-3">
+            {pizzas.map((pizza) => (
+                <CustomCard
+                    key={pizza.id} // Usa un identificador único
+                    name={pizza.name}
+                    desc={pizza.desc}
+                    img={pizza.img}
+                    ingredients={pizza.ingredients}
+                    price={pizza.price}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Pizza;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
-import Header from "../components/Header/Header";
+import Header from "@/components/Header/Header";
+import Pizzas from '@/utils/APIs/Pizzas'
 
 const Home = () => {
 
   return (
     <>
       <Header />
+      <Pizzas/>
     </>
   );
 };
