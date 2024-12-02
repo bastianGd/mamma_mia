8d875c4dca25df0d498f934a6541bed46d9c04ae
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-12-01 22:42:40 -0300

    update link router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import './App.css'
 import RegForm from './views/RegForm'
 import Navbar from './components/Navbars/Navbar'
@@ -6,23 +5,33 @@ import Footer from './components/Footer/FooterPizza'
 import Home from './views/Home'
 import Pizza from './utils/APIs/Pizza'
 import InputLogin from "./views/InputLogin"
+import Cart from "./views/Cart/Cart"
+import Profile from './views/Profile/Profile'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from './views/NotFound/NotFound'
+
 
 function App() {
 
   return (
     <>
-      <div id="root">
-        <div className='main-content'>
-
+      <BrowserRouter>
+        <div id="root">
           <Navbar />
-          <Home/>
-          <Pizza/>
-          <InputLogin/>
-          <RegForm/>
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<InputLogin />} />
+              <Route path="/register" element={<RegForm />} />
+              <Route path="/pizza/p001" element={<Pizza />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/404" element={<NotFound />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-
-      </div>
+      </BrowserRouter>
     </>
   )
 }
